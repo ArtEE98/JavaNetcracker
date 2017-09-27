da98bb1f34156c4ad1aa21de23029743882e0eab
Артем Сидорчук <grandeur19919@gmail.com>
2017-09-27 23:40:14 +0300

    Добавлена тестовая функция для вывода массива и сортировки строк.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        sort(args);
         print(args);
     }
     public static void print(String[] args){
         for (int i=0;i<args.length;i++)
             System.out.println(args[i]);
+    }
+    public static void sort(String[] args){
+        for (int i=0;i<args.length-1;i++)
+            for(int j=i+1;j<args.length;j++)
+                if(args[i].compareTo(args[j])>=0){
+                    String temp=args[i];
+                    args[i]=args[j];
+                    args[j]=temp;
+                }
     }
 }
