2371c1936db88b80824cbeb0a2640debb42521c4
Артем Сидорчук <grandeur19919@gmail.com>
2017-09-28 00:09:00 +0300

    Обновлено согласно JCC

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,17 @@ public class Main {
     }
     
     public static void print(String[] args) {
-        for (int i=0; i<args.length; i++)
+        for (int i = 0; i < args.length; i++)
             System.out.println(args[i]);
     }
     
     public static void sort(String[] args) {
-        for (int i=0; i<args.length-1; i++)
-            for (int j=i+1; j<args.length; j++)
+        for (int i = 0; i < args.length-1; i++)
+            for (int j = i+1; j < args.length; j++)
                 if (args[i].compareTo(args[j]) >= 0) {
-                    swap(args[i], args[j]);
+                    String temp = args[i];
+                    args[i] = args[j];
+                    args[j] = temp;
                 }
-    }
-    public static void swap(string a, string b) {
-        String temp=a;
-        a=b;
-        b=temp;
     }
 }
