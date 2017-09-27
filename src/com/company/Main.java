b22e672fde7346f0af161158e4c0f1b2179bbbee
ArtEE98 <artee.sid98@gmail.com>
2017-09-27 23:54:38 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,21 @@ public class Main {
         print(args);
     }
     
-    public static void print(String[] args){
-        for (int i=0;i<args.length;i++)
+    public static void print(String[] args) {
+        for (int i=0; i<args.length; i++)
             System.out.println(args[i]);
     }
     
-    public static void sort(String[] args){
-        for (int i=0;i<args.length-1;i++)
-            for(int j=i+1;j<args.length;j++)
-                if(args[i].compareTo(args[j])>=0){
-                    String temp=args[i];
-                    args[i]=args[j];
-                    args[j]=temp;
+    public static void sort(String[] args) {
+        for (int i=0; i<args.length-1; i++)
+            for (int j=i+1; j<args.length; j++)
+                if (args[i].compareTo(args[j]) >= 0) {
+                    swap(args[i], args[j]);
                 }
+    }
+    public static void swap(string a, string b) {
+        String temp=a;
+        a=b;
+        b=temp;
     }
 }
